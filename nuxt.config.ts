e959dec170a1765a458205a6dfe1a180842edb24
kelsS <ratherkelsey@gmail.com>
2025-11-19 22:23:12 -0700

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  ssr: false, // SPA mode for better drag performance
+  
   modules: [
     '@nuxt/eslint',
     '@nuxt/hints',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
+    '@nuxt/test-utils',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ],
+  
+  css: ['~/assets/css/main.css'],
+  
+  alias: {
+    'element-resize-detector': '~/shims/element-resize-detector.js'
+  }
 })
